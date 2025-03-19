a3fb0b4b7d99e6839ba3033c7b06698182f1e142
chiyousir <chiyou1216@gmail.com>
2025-03-19 18:37:47 +0800

    Fix font and i18n imports

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,18 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { NextIntlClientProvider } from 'next-intl';
+import { Inter } from 'next/font/google'
+import { NextIntlClientProvider } from 'next-intl'
+import { notFound } from 'next/navigation'
 import "../globals.css";
-import { locales } from "../../i18n";
+import { locales } from '@/src/i18n/settings'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ['latin'] })
 
 export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
@@ -35,13 +28,23 @@ type Props = {
   params: { locale: string };
 };
 
-export default async function LocaleLayout({ children, params }: Props) {
-  const { locale } = params;
-  const messages = (await import(`../../messages/${locale}/index`)).default;
+async function getMessages(locale: string) {
+  try {
+    return (await import(`@/messages/${locale}/index`)).default;
+  } catch (error) {
+    notFound();
+  }
+}
+
+export default async function LocaleLayout({
+  children,
+  params: { locale }
+}: Props) {
+  const messages = await getMessages(locale);
 
   return (
     <html lang={locale}>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={inter.className}>
         <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
         </NextIntlClientProvider>
